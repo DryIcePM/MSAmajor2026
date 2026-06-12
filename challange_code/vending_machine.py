1696914f56626a80fdb6b4fc7672baa581d95e9e
DryIcePM <pwmarsh2010@gmail.com>
2026-06-12 09:08:08 -0500

    Update vending_machine.py

--- challange_code/vending_machine.py
+++ challange_code/vending_machine.py
@@ -1,43 +1,16 @@
-#Input values can only be accepted if they are the numbers 1, 5, 10, or 25
-#Prompt user to enter a coin by detailing the words Vending Machine, separating the text with dashes and then prompting for user input 
-print(f"Vending Machine\n------------------------")
+print(f"\nVending Machine\n------------------------")
 amount_due = 50
+while(True):
+    try: 
+        print(f"Amount due: {amount_due}")
+        coin_inserted = int(input("\nInsert Coin: "))
+        if coin_inserted == 5 or coin_inserted == 1 or coin_inserted == 10 or coin_inserted == 25:
+            amount_due = amount_due - coin_inserted
+            if amount_due <= 0:
+                break
+        else:
+            continue
+    except:
+        continue
 
-
-
-
-
-#make a function so the program validates the input based on the accepted values
-#create a while-loop to continue to prompt the user without subtracting from the total if the input is not valid
-while(true):
-    try:
-        coin_inserted = int(input("Insert Coin"))
-        if coin_inserted != 5 or coin_inserted != 1 or coin_inserted != 10 or coin_inserted != 25:
-        
-            break
-
-
-
-
-
-
-#make a function that subtracts values from the total cost of 50
-#continue until the total amount dued reaches zero
-
-
-
-
-#make a function to grant extra change if the final input puts the total dued below zero.
-
-
-
-
-
-
-
-
-
-
-
-
-main()
+print(f"\nChange: {amount_due * -1}")
