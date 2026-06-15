932eff0f95604c3935b42cb09c3ad3bd8c664932
DryIcePM <pwmarsh2010@gmail.com>
2026-06-15 09:19:40 -0500

    Update math_interpreter.py

--- challange_code/math_interpreter.py
+++ challange_code/math_interpreter.py
@@ -1,36 +1,33 @@
-#while loop
 while True:
-    #Input
-    #prompt user
     expression = input("Enter an expression (X Y Z): ")
-    #Process
-    #validate the expression format 
-    #use the split method to split the expression to at the space
     expression_split = expression.split(" ")
-    #if the length of the resulting list is not 3 then invalid format
     if(len(expression_split) != 3):
-        print(f"Invalid format")
+        print(f"Invalid Format")
         continue
-    #validate that x and z are integers
-    #convert to int
     try:
         x = int(expression_split[0])
         z = int(expression_split[2])
     except: 
-        print(f"Invalid format")
+        print(f"Invalid Format")
         continue
-    #if conversion causes an exception, then incorrect format
-
-    #validate that y is an acceptable operator (+, -, *, /)
-    #use an if statement to determine if y == the symbols
-    #invalid format if not
-
-    #validate that when y = /, z != 0
-    #use if statement: if y == / and z == 0, invalid format
-
-    #do the math
-
-
-    #Output
-    #print the answer
-    #ask to do it again
+    y = expression_split[1]
+    if(y != "+" and y != "-" and y != "/" and y != "*"):
+        print("Invalid Format")
+        continue
+    if(y == "/" and z == 0):
+        print("Divide by Zero Error")
+        continue
+    if(y == "+"):
+        final_answer = (x + z)
+    if(y == "-"):
+        final_answer = (x - z)
+    if(y == "*"):
+        final_answer = (x * z)
+    if(y == "/"):
+        final_answer = (x / z)
+    print(f"Answer = {final_answer}")
+    answer = input("Would You Like To Enter Another Expression? (Y or N) ")
+    if(answer == "Y"):
+       continue
+    else:
+        break
