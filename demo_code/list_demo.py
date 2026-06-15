84905aa3621d29e3234f9c7e187a502ae1a0f697
DryIcePM <pwmarsh2010@gmail.com>
2026-06-15 10:43:39 -0500

    Add files via upload

---
+++ demo_code/list_demo.py
@@ -0,0 +1,97 @@
+def main():
+    #create a list of strings, integers, and different values
+    names = ["John", "Mary", "Alice", "Bob"]
+    list_of_integers = [10, 16, 24, 42, 14, 9]
+    random_type_list = ["Cyd", 15, 22.3, True, "Frank"]
+    empty_list = []
+    
+    #Print a list
+    print(list_of_integers)
+
+    # add values to a list
+    print("\nAdd values to a list\n--------------")
+    names.append("Johnny")
+    list_of_integers.append(63)
+    list_of_integers.append(5)
+    print(f"List of Integers: {list_of_integers}")
+    print(f"List of Names: {names}")
+
+    print("\nGet the number of items in a list\n--------------------")
+    print(f"Items in integer list: {len(list_of_integers)}")
+    print(f"Items in names list: {len(names)}")
+    print(f"Items in empty list: {len(empty_list)}")
+
+    print("\nGet specific values at indexes in a list\n--------------------")
+    print(f"First item in names list: {names[0]}")
+    print(f"Fourth item in names list: {names[3]}")
+
+    #print all items in a list
+    print("\nPrinting all names\n-----------")
+    for name in names:
+        print(name)
+    
+    print("\nPrinting all names with index values\n--------------")
+    for index in range(len(names)):
+        print(f"Names [{index}] -> {names[index]}")
+
+
+    #Calculate the sum of all values in a list
+    sum_of_all_integers = 0
+    for number in list_of_integers:
+        sum_of_all_integers += number
+    
+    print(f"Sum of all integers: {sum_of_all_integers}")
+
+    #does the list contain a specific item
+    search_name = "Veronica"
+    if search_name not in names:
+        print(f"{search_name} is not in the names list")
+    else:
+        print(f"{search_name} is in the names list")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
