5341fd5fe49277984880c0ff2005dc0b0c110553
DryIcePM <pwmarsh2010@gmail.com>
2026-06-18 10:40:49 -0500

    Update adding_game.py

--- challange_code/adding_game.py
+++ challange_code/adding_game.py
@@ -30,16 +30,48 @@ def question_amount():
     return question_number
 
 def main():
+    correct_answers = 0
     #get difficulty level
     difficulty = difficulty_level()
     #get question number
     question_number = question_amount()
     #use a for loop to ask the amount of questions entered in question_number
-    for _ in question_amount:
-        print(random_generator.randint(0, 100))
+    for _ in range(question_number):
         #generate two random numbers based on the difficult
+        if difficulty == 1: 
+            x = random_generator.randint(0, 9)
+            y = random_generator.randint(0, 9)
+        elif difficulty == 2:
+            x = random_generator.randint(10, 99)
+            y = random_generator.randint(10, 99)
+        elif difficulty == 3:
+            x = random_generator.randint(100, 999)
+            y = random_generator.randint(100, 999)
 
+        times_question_asked = 0
 
+        while(True):
+            try:
+                user_answer = int(input(f"{x} + {y} = "))
 
+            except:
+                print("Wrong!!!!!!!!!!!!!!!")
+                times_question_asked += 1
+                continue
+
+            if user_answer != (x + y):
+                print("Wrong!!!!!!!!!!!!!")
+                times_question_asked += 1
+
+            elif user_answer == (x + y):
+                print("Correct")
+                correct_answers += 1
+                break
+
+            if times_question_asked == 3:
+                print(f"Correct answer: {x} + {y} = {x + y}")
+                break
+
+    print(f"You got {correct_answers} questions correct out of {question_number}. {correct_answers / question_number * 100}")
 
 main()
