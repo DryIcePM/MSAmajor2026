610a16a2c2e23eea8ff4031a53ac383bb8d68559
DryIcePM <pwmarsh2010@gmail.com>
2026-06-19 11:31:49 -0500

    Add files via upload

---
+++ demo_code/student_generator.py
@@ -0,0 +1,33 @@
+from Student import Student
+
+def main():
+    #open the csv file
+    data_file = open("students.csv", "r")
+    print(data_file)
+
+    #use a loop to read the file contents line by line
+    for line_of_data in data_file:
+            #split the lines at their commas
+            student_and_student_info = line_of_data.split(",")
+            print(student_and_student_info)
+
+    #get the student and their info from the list
+        item_name = item_name_and_price[0]
+        item_name = item_name_and_price[0]
+        item_name = item_name_and_price[0]
+        item_name = item_name_and_price[0]
+        item_name = item_name_and_price[0]
+        item_name = item_name_and_price[0]
+    #create an entry in the dictionary for the student and their info
+    
+#close the file
+
+
+
+
+
+
+
+
+
+main()
