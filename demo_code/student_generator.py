f5db576e1e3d1106ff3f933bf3e5c7bde8712644
DryIcePM <pwmarsh2010@gmail.com>
2026-06-22 10:57:06 -0500

    Update student_generator.py

--- demo_code/student_generator.py
+++ demo_code/student_generator.py
@@ -1,33 +1,30 @@
 from Student import Student
 
-def main():
+def main(file_name):
+
     #open the csv file
-    data_file = open("students.csv", "r")
+    data_file = open(file_name, "r")
     print(data_file)
+    students = []
 
     #use a loop to read the file contents line by line
     for line_of_data in data_file:
+            
             #split the lines at their commas
             student_and_student_info = line_of_data.split(",")
-            print(student_and_student_info)
-
+            try:
     #get the student and their info from the list
-        item_name = item_name_and_price[0]
-        item_name = item_name_and_price[0]
-        item_name = item_name_and_price[0]
-        item_name = item_name_and_price[0]
-        item_name = item_name_and_price[0]
-        item_name = item_name_and_price[0]
-    #create an entry in the dictionary for the student and their info
-    
-#close the file
+                kid = Student(student_and_student_info[0],student_and_student_info[1],student_and_student_info[2],float(student_and_student_info[3]),float(student_and_student_info[4]),student_and_student_info[5])    
+            except:
+                continue
+        #create an entry in the dictionary for the student and their info
+            students += [kid]
 
+    #close the file
+    data_file.close()
+    return students
+student = main("students.csv")
 
+for kid in student:
+     kid.print_data()
 
-
-
-
-
-
-
-main()
