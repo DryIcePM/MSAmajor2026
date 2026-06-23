f67039e2d2d8d221855db2608588f20cc45120ff
DryIcePM <pwmarsh2010@gmail.com>
2026-06-23 11:27:59 -0500

    Update student_generator.py

--- demo_code/student_generator.py
+++ demo_code/student_generator.py
@@ -1,9 +1,14 @@
 from Student import Student
 
-def main(file_name):
+"""
+Function to return a list of student objects
+input: none
+output: list of student objects
+"""
+def load_students() -> list[Student]:
 
     #open the csv file
-    data_file = open(file_name, "r")
+    data_file = open("students.csv", "r")
     print(data_file)
     students = []
 
@@ -23,8 +28,43 @@ def main(file_name):
     #close the file
     data_file.close()
     return students
-student = main("students.csv")
 
-for kid in student:
-     kid.print_data()
+"""
+Function to convert student objets into student dictionaries
+Input: list of student objects
+output: list of student dictionaries
+"""
+def student_to_dictionary(list_of_students: list[Student]) -> list[dict]:
+    #create an empty list to store the dictionaries
+    student_dictionary_list = []
 
+    #loop through the list and write each students data to a dictionary
+    for student in list_of_students:
+        #create an empty dictionary
+        #make entries into the dictionary using the student properties
+        #firstname, lastname, major, gpa, class, id
+        student_dictionary = {}
+        student_dictionary['first_name'] = student.get_firstname()
+        student_dictionary['last_name'] = student.get_lastname()
+        student_dictionary['major'] = student.get_major()
+        student_dictionary['gpa'] = student.get_gpa()
+        student_dictionary['class_level'] = student.get_class_level()
+        student_dictionary['ID'] = student.get_id()
+
+        #append the dictionary to the list of dictionaries
+        student_dictionary_list.append(student_dictionary)
+    #return the list of dictionaries
+    return student_dictionary_list
+"""
+Function to get student dictionaries
+input:none
+output:a list of student dictionaries
+"""
+def get_student_dictionaries():
+    #get a list of students
+    student_list = load_students()
+
+    #get a list of student dictionaries
+    student_dictionaries = student_to_dictionary(student_list)
+
+    return student_dictionaries
